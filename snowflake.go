6f59d261a646d194d0a81f7f46206f5289e49d58
Tony.Shao <xiocode@gmail.com>
2017-04-14 10:27:06 +0800

    update

--- snowflake.go
+++ snowflake.go
@@ -9,89 +9,81 @@
 package snowflake
 
 import (
+	"errors"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
-var generator *Generator
+const (
+	// custom epoch - time offset, milliseconds
+	// Oct 25, 2014 05:06:02.373 UTC, incidentally equal to the first few digits of sqrt(2)
+	epoch int64 = 1414213562373
 
-func init() {
-	generator = newGenerator(1, 1)
+	// number of bit allocated for server id (max 1023)
+	numWorkerBits = 10
+	// # of bits allocated for millisecond
+	numSequenceBits = 12
+
+	// workerId mask
+	maxWorkerId = -1 ^ (-1 << numWorkerBits)
+	// sequence mask
+	maxSequence = -1 ^ (-1 << numSequenceBits)
+)
+
+type SnowFlake struct {
+	lastTimestamp uint64
+	sequence      uint32
+	workerId      uint32
+	lock          sync.Mutex
 }
 
-func GetUUID() int64 {
-	return generator.next()
+// pack bits into a snowflake
+func (sf *SnowFlake) pack() uint64 {
+	return (sf.lastTimestamp << (numWorkerBits + numSequenceBits)) |
+		(uint64(sf.workerId) << numSequenceBits) |
+		(uint64(sf.sequence))
 }
 
-type Generator struct {
-	workerId           int64
-	dataCenterId       int64
-	idsGenerated       int64
-	currentTimeStamp   int64
-	sequence           int64
-	workerIdBits       uint
-	dataCenterIdBits   uint
-	maxWorkerId        int64
-	max_data_center_id int64
-	sequenceBits       uint
-	workerIdShift      uint
-	dataCenterIdShift  uint
-	timeStampLeftShift uint
-	sequenceMask       int64
-	lastTimeStamp      int64
-	sync.Mutex
-}
-
-func newGenerator(workerId, dataCenterId int64) *Generator {
-	Generator := &Generator{}
-	Generator.workerId = workerId
-	Generator.dataCenterId = dataCenterId
-	Generator.idsGenerated = 0
-	Generator.currentTimeStamp = 1318323746000
-	Generator.sequence = 0
-	Generator.workerIdBits = 5
-	Generator.dataCenterIdBits = 5
-	Generator.sequenceBits = 12
-	Generator.maxWorkerId = -1 ^ (-1 << Generator.workerIdBits)
-	Generator.max_data_center_id = -1 ^ (-1 << Generator.dataCenterIdBits)
-	Generator.workerIdShift = Generator.sequenceBits
-	Generator.dataCenterIdShift = Generator.sequenceBits + Generator.workerIdBits
-	Generator.timeStampLeftShift = Generator.sequenceBits + Generator.workerIdBits + Generator.dataCenterIdBits
-	Generator.sequenceMask = -1 ^ (-1 << Generator.sequenceBits)
-	Generator.lastTimeStamp = -1
-	return Generator
-}
-
-func (this *Generator) now() int64 {
-	return time.Now().Unix() * 1000
-}
-
-func (this *Generator) nextTime(lastTimeStamp int64) int64 {
-	timeStamp := this.now()
-	for timeStamp == lastTimeStamp {
-		timeStamp = this.now()
+// Initialise generator
+func NewSnowFlake(workerId uint32) (*SnowFlake, error) {
+	if workerId < 0 || workerId > maxWorkerId {
+		return nil, errors.New("invalid worker Id")
 	}
-	return timeStamp
+	return &SnowFlake{workerId: workerId}, nil
 }
 
-func (this *Generator) next() int64 {
-	this.Lock()
-	defer this.Unlock()
+// Generate next unique ID
+func (sf *SnowFlake) Next() (uint64, error) {
+	sf.lock.Lock()
+	defer sf.lock.Unlock()
 
-	timeStamp := this.now()
-	if this.lastTimeStamp > timeStamp {
-		return 0
-	}
-	if this.lastTimeStamp == timeStamp {
-		this.sequence = (atomic.AddInt64(&this.sequence, 1)) & this.sequenceMask
-		if this.sequence == 0 {
-			timeStamp = this.nextTime(this.lastTimeStamp)
+	ts := timestamp()
+	if ts == sf.lastTimestamp {
+		sf.sequence = (sf.sequence + 1) & maxSequence
+		if sf.sequence == 0 {
+			ts = sf.waitNextMilli(ts)
 		}
 	} else {
-		this.sequence = 0
+		sf.sequence = 0
 	}
-	this.lastTimeStamp = timeStamp
 
-	return ((timeStamp - this.currentTimeStamp) << this.timeStampLeftShift) | (this.dataCenterId << this.dataCenterIdShift) | (this.workerId << this.workerIdShift) | this.sequence
+	if ts < sf.lastTimestamp {
+		return 0, errors.New("invalid system clock")
+	}
+	sf.lastTimestamp = ts
+	return sf.pack(), nil
+}
+
+// Sequance exhausted. Wait till next millisocond
+func (sf *SnowFlake) waitNextMilli(ts uint64) uint64 {
+	for ts == sf.lastTimestamp {
+		time.Sleep(100 * time.Microsecond)
+		ts = timestamp()
+	}
+	return ts
+}
+
+func timestamp() uint64 {
+	// convert from nanoseconds to milliseconds, adjust for custom epoch
+	return uint64(time.Now().UnixNano()/int64(time.Millisecond) - epoch)
 }
